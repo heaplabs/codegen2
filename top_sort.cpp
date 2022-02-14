5ac0c8cf121d6e282a8913750eabdaf97c4984e6
neil.xavier.dsouza <neil.xavier.dsouza@gmail.com>
2022-02-14 16:32:35 +0530

    refactor

--- top_sort.cpp
+++ top_sort.cpp
@@ -15,45 +15,21 @@ struct Graph {
 	{ } 
 	map<string, set<string>> node_relations;
 
-	void topological_sort(
+	void topological_sort_aux(
 			string node,
 			deque<pair<string, int> > & order,
 			map<string, bool> & visited,
 			deque<string> & visit_queue,
 			int nest_level)
 	{
-		cout << "ENTER topological_sort: " 
+		cout << "ENTER topological_sort_aux: " 
 			<< " nest_level: " << nest_level << endl;
 		if (nest_level > 20) {
 			return;
 		}
 
-		if (node == "") {
-			cout << "INFO queue empty - searching for a starting node" << endl;
-			for (map<string, set<string>>::iterator it = 
-				node_relations.begin(); it != node_relations.end();
-				++it) {
-				if (!visited[it->first]) {
-					cout << "processing node: " << it->first << ", nest_level: " << nest_level << endl;
-					set<string>& nodes = it->second;
-					for (set<string>::const_iterator n_it = 
-						nodes.begin(); n_it != nodes.end();
-						++n_it) {
-						if (!visited[*n_it]) {
-							cout << "pushing " << *n_it << " into the queue" << endl;
-							//visit_queue.push_back(*n_it);
-							topological_sort(*n_it, order, visited, visit_queue, nest_level+1);
-							visited[*n_it] = true;
-						}
-					}
-					//topological_sort(order, visited, visit_queue, nest_level+1);
-					order.push_back({it->first, nest_level});
-					visited[it->first] = true;
-				}
-			}
-		} else {
 			//string node = visit_queue.front();
-			cout << "INFO topological_sort visit_queue head: " << node
+			cout << "INFO topological_sort_aux visit_queue head: " << node
 				<< endl;
 			//visit_queue.pop_front();
 			if (!visited[node]) {
@@ -63,16 +39,49 @@ struct Graph {
 					++n_it) {
 					if (!visited[*n_it]) {
 						//visit_queue.push_back(*n_it);
-						topological_sort(*n_it, order, visited, visit_queue, nest_level+1);
+						topological_sort_aux(*n_it, order, visited, visit_queue, nest_level+1);
 						visited[*n_it] = true;
 					}
 				}
-				//topological_sort(order, visited, visit_queue, nest_level+1);
+				//topological_sort_aux(order, visited, visit_queue, nest_level+1);
 				cout << "adding node: " << node << " to the topological order" << endl;
 				order.push_back({node, nest_level});
 				visited[node] = true;
 			}
+	}
+
+	deque<pair<string, int> > topological_sort() {
+		deque<pair<string, int> >  order;
+		map<string, bool>  visited;
+		deque<string> visit_queue;
+		for ( map<string, set<string> >::const_iterator it = 
+			node_relations.begin(); it != node_relations.end(); ++it ) {
+			visited.insert({it->first, false});
 		}
+		int nest_level = 1;
+		cout << "INFO queue empty - searching for a starting node" << endl;
+		for (map<string, set<string>>::iterator it = 
+			node_relations.begin(); it != node_relations.end();
+			++it) {
+			if (!visited[it->first]) {
+				cout << "processing node: " << it->first << ", nest_level: " << nest_level << endl;
+				set<string>& nodes = it->second;
+				for (set<string>::const_iterator n_it = 
+					nodes.begin(); n_it != nodes.end();
+					++n_it) {
+					if (!visited[*n_it]) {
+						cout << "pushing " << *n_it << " into the queue" << endl;
+						//visit_queue.push_back(*n_it);
+						topological_sort_aux(*n_it, order, visited, visit_queue, nest_level+1);
+						visited[*n_it] = true;
+					}
+				}
+				//topological_sort_aux(order, visited, visit_queue, nest_level+1);
+				order.push_back({it->first, nest_level});
+				visited[it->first] = true;
+			}
+		}
+		return order;
 	}
 };
 
@@ -104,14 +113,8 @@ int main() {
 	graph.insert({"continuous_fns", continuous_fns_deps});
 	graph.insert({"real_line", real_line_deps});
 	Graph g(graph);
-	deque<pair<string, int> >  order;
-	map<string, bool>  visited;
-	deque<string> visit_queue;
-	for ( map<string, set<string> >::const_iterator it = 
-		graph.begin(); it != graph.end(); ++it ) {
-		visited.insert({it->first, false});
-	}
-	g.topological_sort(string(),  order, visited, visit_queue, 1);
+
+	deque<pair<string, int> >  order = g.topological_sort();
 
 	for (int i = 0; i < order.size(); ++i) {
 		cout << order[i].first << ", " << order[i].second << endl;
